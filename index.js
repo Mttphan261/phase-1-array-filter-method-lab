3d9472494bff28793082bf8dc22d9e9e864b694a
Matt Phan <mttphan261@gmail.com>
2023-04-11 19:14:55 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const findMatching = function(array, name) {
+    let driverMatch = array.filter(item => item.toLowerCase() === name.toLowerCase());
+    return driverMatch;
+    };
+
+const fuzzyMatch = function(array, name) {
+    let driverMatch =  array.filter(item => item.charAt(0) == name.charAt(0));
+    return driverMatch;
+    };
+
+const matchName = function(array, str) {
+    let driverMatch = array.filter(item => item.name === str);
+    return driverMatch;
+    };
